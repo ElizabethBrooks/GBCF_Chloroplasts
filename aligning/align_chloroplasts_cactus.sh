3be44cd39904770a891f1a7accd2d344ea28bf5a
ElizabethBrooks <merimaat1@gmail.com>
2024-08-19 14:22:50 -0400

    Updated cactus script and inputs

--- aligning/align_chloroplasts_cactus.sh
+++ aligning/align_chloroplasts_cactus.sh
@@ -7,27 +7,33 @@
 # script to run cactus
 # usage: qsub align_chloroplasts_cactus.sh
 
-# activate python environment
-source /scratch365/ebrooks5/cactus/cactus_env/bin/activate
+# retrieve software path
+softEnv=$(grep "cactus_env:" ../"inputs/software_HPC.txt" | tr -d " " | sed "s/cactus_env://g")
+
+# retrieve software environment
+softPath=$(grep "cactus:" ../"inputs/software_HPC.txt" | tr -d " " | sed "s/cactus://g")
 
 # retrieve inputs
-inputsPath=$(grep "long:" ../"inputs/inputs_HPC.txt" | tr -d " " | sed "s/long://g")
+inputsPath=$(grep "cactus:" ../"inputs/inputs_HPC.txt" | tr -d " " | sed "s/cactus://g")
 
 # retrieve analysis outputs absolute path
 outputsPath=$(grep "outputs:" ../"inputs/inputs_HPC.txt" | tr -d " " | sed "s/outputs://g")
 
 # make a new directory for analysis
-alignOut=$outputsPath"/aligned"
+alignOut=$outputsPath"/aligned_cactus"
 mkdir $alignOut
 
 # move to the new directory
-cd $alignOut
+cd $softPath
+
+# activate python environment
+source $softEnv
 
 # status message
 echo "Beginning analysis..."
 
 # To run:
-cactus ./js ./examples/evolverMammals.txt ./evolverMammals.hal --binariesMode singularity 
+cactus ./js $inputsPath $alignOut"/aligned_test.hal" --binariesMode singularity 
 
 # status message
 echo "Analysis complete!"
