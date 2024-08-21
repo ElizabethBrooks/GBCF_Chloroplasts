6e8ad51f549f75618e295119b5b1889cebde25ab
ElizabethBrooks <merimaat1@gmail.com>
2024-08-20 20:31:50 -0400

    Adjusted script for renaming genomes and added longest inputs for cactus

--- Filtering/subset_genomes_renamed.sh
+++ Filtering/subset_genomes_renamed.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# script to subset genome sequences from a fasta file
-# usage: bash subset_genomes_renamed.sh inputFile
-# usage: bash subset_genomes_renamed.sh /Users/bamflappy/GBCF/JRS/chloroplast/Juglanschlorosaln_long.flt.fa
+# script to subset and rename genome sequences
+# usage: bash subset_genomes_renamed.sh
 
 # retrieve input file
-inputFile=$1
+inputFile="/Users/bamflappy/GBCF/JRS/chloroplast/Juglanschlorosaln_long.flt.fa"
 
 # set output file dir
 outDir=$(dirname $inputFile)
 outDir=$outDir"/chloroplast_genomes_renamed"
+
+# retrieve indexing file
+indexFile="/Users/bamflappy/GBCF/JRS/chloroplast/GBCF_JRS_chloroplasts_IDindicies.csv"
 
 # make genomes subset dir
 mkdir $outDir 
@@ -18,8 +20,8 @@ mkdir $outDir
 while read line; do
 	# determine if current line is a header or body
 	if echo $line | grep -q ">"; then # header
-		# create file name
-		outName=$(($outName + 1))
+		# retrieve genome index
+		outName=$(cat $indexFile | grep "\"$line\"" | cut -d"," -f1)
 	else # body
 		# output genome sequence header and body
 		echo ">"$outName > $outDir"/"$outName".fa"
